477fbc11b6c88fa1dd79a2b0d5038bb852aace69
Unknown <eduard.castro@placetopay.com>
2019-01-26 15:29:45 -0500

    Refactor Reformat code

--- src/main/java/coop/tecso/examen/model/AbstractPersistentObject.java
+++ src/main/java/coop/tecso/examen/model/AbstractPersistentObject.java
@@ -25,107 +25,92 @@ public abstract class AbstractPersistentObject implements Serializable {
 	private static final long serialVersionUID = -975560023284258938L;
 
 	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
-    @Version
-    private long versionNumber;
-    
-    @Temporal(TemporalType.TIMESTAMP)
-    @CreatedDate
-    private Date creationTimestamp;
-    
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    @LastModifiedDate
-    private Date modificationTimestamp;
+	@Version
+	private long versionNumber;
 
+	@Temporal(TemporalType.TIMESTAMP)
+	@CreatedDate
+	private Date creationTimestamp;
 
+	@Temporal(TemporalType.TIMESTAMP)
+	@LastModifiedDate
+	private Date modificationTimestamp;
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public long getVersionNumber() {
+		return versionNumber;
+	}
 
+	public void setVersionNumber(long versionNumber) {
+		this.versionNumber = versionNumber;
+	}
 
-    public long getVersionNumber() {
-        return versionNumber;
-    }
+	public Date getModificationTimestamp() {
+		return modificationTimestamp;
+	}
 
+	public void setModificationTimestamp(Date modificationTimestamp) {
+		this.modificationTimestamp = modificationTimestamp;
+	}
 
-    public void setVersionNumber(long versionNumber) {
-        this.versionNumber = versionNumber;
-    }
+	public Date getCreationTimestamp() {
+		return creationTimestamp;
+	}
 
+	public void setCreationTimestamp(Date creationTimestamp) {
+		this.creationTimestamp = creationTimestamp;
+	}
 
-    public Date getModificationTimestamp() {
-        return modificationTimestamp;
-    }
+	@Override
+	public boolean equals(Object other) {
+		if (super.equals(other)) {
+			return true;
+		}
 
+		if (other == null) {
+			return false;
+		}
 
-    public void setModificationTimestamp(Date modificationTimestamp) {
-        this.modificationTimestamp = modificationTimestamp;
-    }
+		if (this.getClass() != other.getClass()) {
+			return false;
+		}
 
+		AbstractPersistentObject persistentObject = (AbstractPersistentObject) other;
 
-    public Date getCreationTimestamp() {
-        return creationTimestamp;
-    }
+		return getId() != null && getId().equals(persistentObject.getId());
+	}
 
+	@Override
+	public int hashCode() {
+		if (getId() != null) {
+			return this.getClass().hashCode() + getId().hashCode();
+		}
 
-    public void setCreationTimestamp(Date creationTimestamp) {
-        this.creationTimestamp = creationTimestamp;
-    }
+		return super.hashCode();
+	}
 
-    
-    @Override
-    public boolean equals(Object other) {
-        if (super.equals(other)) {
-            return true;
-        }
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(this.getClass());
+		sb.append(" ID:");
+		sb.append(id);
 
-        if (other == null) {
-            return false;
-        }
+		return sb.toString();
+	}
 
-        if (this.getClass() != other.getClass()) {
-            return false;
-        }
-
-        AbstractPersistentObject persistentObject = (AbstractPersistentObject) other;
-        return getId() != null && getId().equals(persistentObject.getId());
-
-    }
-
-
-    @Override
-    public int hashCode() {
-
-        if (getId() != null) {
-            return this.getClass().hashCode() + getId().hashCode();
-        } else {
-            return super.hashCode();
-        }
-    }
-
-
-    @Override
-    public String toString() {
-
-        StringBuilder sb = new StringBuilder();
-        sb.append(this.getClass());
-        sb.append(" ID:");
-        sb.append(id);
-        return sb.toString();
-    }
-
-    @Transient
-    public String getDescription() {
-        return toString();
-    }
-
+	@Transient
+	public String getDescription() {
+		return toString();
+	}
 }
